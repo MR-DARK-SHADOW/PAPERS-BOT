d4082e7674a1af2e6d55f9d0f43dd05ac9bc7761
DARK-SHADOW <124597615+MR-DARK-SHADOW@users.noreply.github.com>
2024-10-20 17:18:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
+ const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "6VcnQT7C#oaz95GDk9f_L0D2wZNj4VnLZlIYnkbX00XPwBCrajl8",
 };
