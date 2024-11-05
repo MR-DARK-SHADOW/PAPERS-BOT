e64dc965f84cd18f0dfe50e2012d7a3d4909c9e5
DARK-SHADOW <124597615+MR-DARK-SHADOW@users.noreply.github.com>
2024-11-05 13:59:47 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KIFACZwZ#6Nwm__7jiB4k96AK7zlf2ia2bzAsIGdAFcMKECzVv-0",
+SESSION_ID: process.env.SESSION_ID,
 };
