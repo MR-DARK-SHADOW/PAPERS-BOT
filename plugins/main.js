b9aca027bd55984d39d3cd75aa64b326c3f0aa16
DARK-SHADOW <124597615+MR-DARK-SHADOW@users.noreply.github.com>
2024-11-10 19:58:04 +0530

    Update main.js

--- plugins/main.js
+++ plugins/main.js
@@ -70,3 +70,87 @@ await conn.sendMessage(from, { react: { text: '✅', key: mek.key }})
     console.log(e);
   }
 });
+
+//=================================================================================================================
+cmd({
+on: "body"
+}, async (conn, mek, m, { from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+  try {
+    if (body === '2') {
+      if (!m.quoted) return;
+      if (m.quoted.sender !== botNumber2) return;
+      if (m.quoted.videoMessage) return;
+      if (m.quoted.documentMessage) return;
+      if (m.quoted.stickerMessage) return;   
+      if (m.quoted.audioMessage) return;
+      if (!m.quoted.imageMessage) return;
+      if (m.quoted.imageMessage.caption.includes("Hello")) {
+
+await conn.sendMessage(from, { react: { text: '🔎', key: mek.key }})
+
+let react = await conn.sendMessage(from, { image: { url: `https://www.newswire.lk/wp-content/uploads/2022/01/AL-Exam.jpg` }, caption: `────────────────────\n> ⭕ *භෞතික විද්‍යා විෂය ධාරාව*\n────────────────────\n_👇🏻 පහතින් ඔබට අවශ්‍ය විශය තෝරා එයට අදාල අංකය මෙය මෙන්ශන් කර ලභා දෙන්න_\n────────────────────\n> 1️⃣ *2020*\n\n> 2️⃣ *2021*\n\n> 3️⃣ *2022*\n\n> 4️⃣ *2023 (2024)*\n\n_© 𝙿𝙾𝚆𝙴𝚁𝙳 𝙱𝚈 𝙳𝙰𝚁𝙺 𝚂𝙷𝙰𝙳𝙾𝚆_\n` }, { quoted: mek })    
+await conn.sendMessage(from, { react: { text: '🗞️', key: react.key } });
+await conn.sendMessage(from, { react: { text: '✅', key: mek.key }})
+      }
+    }
+} catch (e) {
+    reply(`${e}`);
+    console.log(e);
+  }
+});
+
+//=================================================================================================================
+cmd({
+on: "body"
+}, async (conn, mek, m, { from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+  try {
+    if (body === '3') {
+      if (!m.quoted) return;
+      if (m.quoted.sender !== botNumber2) return;
+      if (m.quoted.videoMessage) return;
+      if (m.quoted.documentMessage) return;
+      if (m.quoted.stickerMessage) return;   
+      if (m.quoted.audioMessage) return;
+      if (!m.quoted.imageMessage) return;
+      if (m.quoted.imageMessage.caption.includes("Hello")) {
+
+await conn.sendMessage(from, { react: { text: '🔎', key: mek.key }})
+
+let react = await conn.sendMessage(from, { image: { url: `https://www.newswire.lk/wp-content/uploads/2022/01/AL-Exam.jpg` }, caption: `────────────────────\n> ⭕ *භෞතික විද්‍යා විෂය ධාරාව*\n────────────────────\n_👇🏻 පහතින් ඔබට අවශ්‍ය විශය තෝරා එයට අදාල අංකය මෙය මෙන්ශන් කර ලභා දෙන්න_\n────────────────────\n> 1️⃣ *2020*\n\n> 2️⃣ *2021*\n\n> 3️⃣ *2022*\n\n> 4️⃣ *2023 (2024)*\n\n_© 𝙿𝙾𝚆𝙴𝚁𝙳 𝙱𝚈 𝙳𝙰𝚁𝙺 𝚂𝙷𝙰𝙳𝙾𝚆_\n` }, { quoted: mek })    
+await conn.sendMessage(from, { react: { text: '🗞️', key: react.key } });
+await conn.sendMessage(from, { react: { text: '✅', key: mek.key }})
+      }
+    }
+} catch (e) {
+    reply(`${e}`);
+    console.log(e);
+  }
+});
+
+//=================================================================================================================
+cmd({
+on: "body"
+}, async (conn, mek, m, { from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+  try {
+    if (body === '4') {
+      if (!m.quoted) return;
+      if (m.quoted.sender !== botNumber2) return;
+      if (m.quoted.videoMessage) return;
+      if (m.quoted.documentMessage) return;
+      if (m.quoted.stickerMessage) return;   
+      if (m.quoted.audioMessage) return;
+      if (!m.quoted.imageMessage) return;
+      if (m.quoted.imageMessage.caption.includes("Hello")) {
+
+await conn.sendMessage(from, { react: { text: '🔎', key: mek.key }})
+
+let react = await conn.sendMessage(from, { image: { url: `https://www.newswire.lk/wp-content/uploads/2022/01/AL-Exam.jpg` }, caption: `────────────────────\n> ⭕ *භෞතික විද්‍යා විෂය ධාරාව*\n────────────────────\n_👇🏻 පහතින් ඔබට අවශ්‍ය විශය තෝරා එයට අදාල අංකය මෙය මෙන්ශන් කර ලභා දෙන්න_\n────────────────────\n> 1️⃣ *2020*\n\n> 2️⃣ *2021*\n\n> 3️⃣ *2022*\n\n> 4️⃣ *2023 (2024)*\n\n_© 𝙿𝙾𝚆𝙴𝚁𝙳 𝙱𝚈 𝙳𝙰𝚁𝙺 𝚂𝙷𝙰𝙳𝙾𝚆_\n` }, { quoted: mek })    
+await conn.sendMessage(from, { react: { text: '🗞️', key: react.key } });
+await conn.sendMessage(from, { react: { text: '✅', key: mek.key }})
+      }
+    }
+} catch (e) {
+    reply(`${e}`);
+    console.log(e);
+  }
+});
