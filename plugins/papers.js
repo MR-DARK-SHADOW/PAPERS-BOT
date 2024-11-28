9f45a689d0018165994bad6397278ccbf7793f36
DARK-SHADOW <124597615+MR-DARK-SHADOW@users.noreply.github.com>
2024-11-28 22:51:33 +0530

    Create papers.js

---
+++ plugins/papers.js
@@ -0,0 +1,31 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
+
+
+//=================================================================================================================
+cmd({
+on: "body"
+}, async (conn, mek, m, { from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+  try {
+    if (body === '1') {
+      if (!m.quoted) return;
+      if (m.quoted.sender !== botNumber2) return;
+      if (m.quoted.videoMessage) return;
+      if (m.quoted.documentMessage) return;
+      if (m.quoted.stickerMessage) return;   
+      if (m.quoted.audioMessage) return;
+      if (!m.quoted.imageMessage) return;
+      if (m.quoted.imageMessage.caption.includes("⭕ *ගණිතය*")) {
+
+await conn.sendMessage(from, { react: { text: '🔎', key: mek.key }})
+let react = await conn.sendMessage(from, { document: { url: `https://github.com/MR-DARK-SHADOW/AL-PAST-PAPERS-DB/blob/main/2016-OL-Maths-Past-Paper-and-Answers-Sinhala-Medium.pdf?raw=true` }, mimetype: "application/pdf", fileName: `ගණිතය_2016.pdf`, caption: `> *Have a nice day 🌝❤️\n\n_© 𝙿𝙾𝚆𝙴𝚁𝙳 𝙱𝚈 𝙳𝙰𝚁𝙺 𝚂𝙷𝙰𝙳𝙾𝚆_` }, { quoted: mek  })
+await conn.sendMessage(from, { react: { text: '🗞️', key: react.key } });
+await conn.sendMessage(from, { react: { text: '✅', key: mek.key }})
+      }
+    }
+} catch (e) {
+    reply(`${e}`);
+    console.log(e);
+  }
+});
