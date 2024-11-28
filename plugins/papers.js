3f4bf306748cae0b1ff0f6569489ebd153b1950e
DARK-SHADOW <124597615+MR-DARK-SHADOW@users.noreply.github.com>
2024-11-28 23:13:32 +0530

    Update papers.js

--- plugins/papers.js
+++ plugins/papers.js
@@ -19,7 +19,7 @@ on: "body"
       if (m.quoted.imageMessage.caption.includes("⭕ *ගණිතය*")) {
 
 await conn.sendMessage(from, { react: { text: '🔎', key: mek.key }})
-let react = await conn.sendMessage(from, { document: { url: `https://fv5-3.files.fm/down.php?cf&i=d9avrjw6zb&n=2016-OL-Maths-Past-Paper-and-Answers-Sinhala-Medium.pdf` }, mimetype: "application/pdf", fileName: `ගණිතය_2016.pdf`, caption: `> *Have a nice day* 🌝❤️\n\n_© 𝙿𝙾𝚆𝙴𝚁𝙳 𝙱𝚈 𝙳𝙰𝚁𝙺 𝚂𝙷𝙰𝙳𝙾𝚆_` }, { quoted: mek  })
+let react = await conn.sendMessage(from, { document: { url: `https://github.com/MR-DARK-SHADOW/AL-PAST-PAPERS-DB/blob/main/2016-OL-Maths-Past-Paper-and-Answers-Sinhala-Medium.pdf?raw=` }, mimetype: "application/pdf", fileName: `ගණිතය_2016.pdf`, caption: `> *Have a nice day* 🌝❤️\n\n_© 𝙿𝙾𝚆𝙴𝚁𝙳 𝙱𝚈 𝙳𝙰𝚁𝙺 𝚂𝙷𝙰𝙳𝙾𝚆_` }, { quoted: mek  })
 await conn.sendMessage(from, { react: { text: '🗞️', key: react.key } });
 await conn.sendMessage(from, { react: { text: '✅', key: mek.key }})
       }
