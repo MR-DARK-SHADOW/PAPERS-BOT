c0dfc4db6839c09aa9a7902c9c9dc84abc767956
DARK-SHADOW <124597615+MR-DARK-SHADOW@users.noreply.github.com>
2025-01-27 10:07:32 +0530

    Update config.j

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "FzFjAa7S#WXhOHzL7dFYLlvklwnY_KpIU6MNFki5CM5gRboEUuTo",
+SESSION_ID: process.env.SESSION_ID || "YKEWBB7J#MRlGuk2P73SzgUW_LK8TzTOYAgu4focKhd_BhRw7Jbk",
 };
